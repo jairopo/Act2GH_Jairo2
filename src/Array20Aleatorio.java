94330d26ffdd8fd2801c1835d41701a69d6b18b4
Jairo Andrades <jairo.po8@gmail.com>
2023-01-20 14:32:14 +0100

    Creado array de 20 números

---
+++ src/Array20Aleatorio.java
@@ -0,0 +1,22 @@
+
+public class Array20Aleatorio {
+
+public static void main(String[] args) {
+    
+    final int LONGITUDARRAY = 20;
+    final int RANGOINICIO = 1;
+    final int RANGOFIN = 10;
+    int[] numerosAleatorios = new int[LONGITUDARRAY];
+    
+    for (int i = 0; i < LONGITUDARRAY; i++) {
+      numerosAleatorios[i] = (int) (Math.random() * (RANGOFIN - RANGOINICIO + 1) + RANGOINICIO);
+    }
+    
+    System.out.println("Contenido del array");
+    for (int i = 0; i < LONGITUDARRAY; i++) {
+      System.out.print(numerosAleatorios[i] + " ");
+    }    
+       
+  }
+  
+}
